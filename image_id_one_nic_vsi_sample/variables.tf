cce11c482ad9aec800aa581080d9c0b152fae40c
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2021-02-27 18:54:17 +0530

    Added new folder for VSI creation using image id

---
+++ image_id_one_nic_vsi_sample/variables.tf
@@ -0,0 +1,67 @@
+##############################################################################
+# Variable block - See each variable description
+##############################################################################
+
+
+##############################################################################
+# subnet_id - Subnet where resources are to be provisioned.
+##############################################################################
+variable "subnet_id"{
+  default = ""
+  description = "The id of the subnet to which VSI's interface belongs to"
+}
+
+##############################################################################
+# ssh_key_name - The name of the public SSH key to be used when provisining VSI.
+##############################################################################
+variable "ssh_key_name" {
+  default     = ""
+  description = "The name of the public SSH key to be used when provisining VSI."
+}
+
+##############################################################################
+# vnf_vpc_image_name - The name of the catalog image to be provisioned in your IBM Cloud account.
+##############################################################################
+
+variable "vnf_image_id" {
+  default = ""
+  description = "The id of the catalog image to be provisioned in your IBM Cloud account."
+}
+
+##############################################################################
+# vnf_vpc_instance_name - The name of your Virtual Server to be provisioned
+##############################################################################
+variable "vnf_instance_name" {
+  default     = "ubuntu18-04-vsi"
+  description = "The name of your Virtual Server to be provisioned."
+}
+
+##############################################################################
+# vnf_profile - The profile of compute CPU and memory resources to be used when provisioning VSI.
+##############################################################################
+variable "vnf_profile" {
+  default     = "bx2-2x8"
+  description = "The profile of compute CPU and memory resources to be used when provisioning VSI. To list available profiles, run `ibmcloud is instance-profiles`."
+}
+
+variable "region" {
+  default     = "us-south"
+  description = "Optional. The value of the region of VPC."
+}
+
+#####################################################################################################
+# api_key - This is the ibm_cloud_api_key which should be used only while testing this code from CLI. 
+# It is not needed while testing from Schematics
+######################################################################################################
+/*variable "api_key" {
+  default     = ""
+  description = "holds the user api key"
+}*/
+
+##############################################################################
+# vnf_securtiy_group - The security group to which the VSI interface belongs to.
+##############################################################################
+variable "vnf_security_group" {
+  default     = "ubuntu-security-group"
+  description = "The security group for VNF VPC"
+}
