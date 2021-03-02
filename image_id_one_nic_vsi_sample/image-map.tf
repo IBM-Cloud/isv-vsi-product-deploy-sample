0cabb1eeabc830f58054da62cdc928012d0cc2fc
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2021-03-02 23:23:08 +0530

    Added for image map

---
+++ image_id_one_nic_vsi_sample/image-map.tf
@@ -0,0 +1,11 @@
+locals {
+        image_map = {
+	
+        "us-south" = "r134-ffa6a43c-9466-4fa9-823f-87c0d21c81a9"
+	"us-east" = "r014-eccb5c62-82d9-438c-b81e-716f3506700f"
+	"eu-gb" = "r018-72ee97b8-ffeb-4427-bd2a-fc60e4d2b6b5"
+        "eu-de" = "r010-cf56a548-d5ca-4833-b0a6-bde256140d93"
+	"jp-tok" = "r022-44656c7d-427c-4e06-9253-3224cd1df827"	
+	"eu-fr2" = "r030-e75c31f5-0372-4b1f-a9a9-75b0b147b128"
+        }
+}
