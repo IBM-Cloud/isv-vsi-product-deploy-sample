4b20b7480df187339c7a784843c968685f2c434c
MalarvizhiK <k.malarvizhi@in.ibm.com>
2021-03-03 23:51:41 +0530

    Update main.tf

--- custom_cos_image_one_nic_vsi_sample/main.tf
+++ custom_cos_image_one_nic_vsi_sample/main.tf
@@ -87,6 +87,25 @@ resource "ibm_is_security_group_rule" "vsi_sg_rule_out_all" {
   direction = "outbound"
   remote    = "0.0.0.0/0"
 }
+  
+##############################################################################
+# This creates custom image using qcow2 image hosted in COS
+##############################################################################
+# Generating random ID
+resource "random_uuid" "test" { }
+
+resource "ibm_is_image" "vsi_custom_image" {
+  depends_on       = [random_uuid.test]
+  href             = var.vsi_cos_image_url
+  name             = "${var.vsi_vpc_image_name}-${substr(random_uuid.test.result,0,8)}"
+  operating_system = "ubuntu-18-04-amd64"
+  resource_group = data.ibm_is_subnet.vsi_subnet.resource_group
+
+  timeouts {
+    create = "30m"
+    delete = "10m"
+  }
+}
 
 //vsi instance 
 resource "ibm_is_instance" "sample_vsi" {
