eae8948d25497f026ef7ad4b9eac36ea8d8dbf88
Malar Kandasamy <k.malarvizhi@in.ibm.com>
2021-04-14 20:13:58 +0530

    Retrieve image by id based on region

--- image-map.tf
+++ image-map.tf
@@ -2,5 +2,11 @@ locals {
         image_map = {
 			"us-south" = "r006-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
 			"us-east" = "r014-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
+			"eu-gb" = "r018-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
+			"eu-de" = "r010-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
+			"jp-tok" = "r022-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
+			"jp-osa" = "r034-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
+			"au-syd" = "r026-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
+			"ca-tor" = "r038-ed3f775f-ad7e-4e37-ae62-7199b4988b00"
         }
 }
